6df0d6b98a8af575a74294cd772f2b5f3dd3a52d
huai zhu <138862916+riceshowerX@users.noreply.github.com>
2024-05-21 16:03:01 +0800

    1.1.7
    fix

--- app/core/config/config.py
+++ app/core/config/config.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings
 import os
 
 class Settings(BaseSettings):
-    SERVER_HOST: str = os.getenv("SERVER_HOST", "0.0.0.0")
+    SERVER_HOST: str = os.getenv("SERVER_HOST", "127.0.0.1")
     SERVER_PORT: int = int(os.getenv("SERVER_PORT", 8015))
     CORS_ORIGINS: list =  os.getenv("CORS_ORIGINS", "http://localhost:8501").split(',')
     API_KEY: str = os.getenv("API_KEY")
