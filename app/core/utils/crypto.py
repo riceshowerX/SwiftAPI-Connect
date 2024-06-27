57a4f62eceee1bd6c297967b58e8fe9dd8ed5e9e
huai zhu <138862916+riceshowerX@users.noreply.github.com>
2024-06-27 15:19:19 +0800

    1.3.2

--- app/core/utils/crypto.py
+++ app/core/utils/crypto.py
@@ -1,5 +1,4 @@
 # crypto.py
-# crypto.py
 from cryptography.fernet import Fernet, InvalidToken
 from typing import Union
 import os
@@ -11,10 +10,10 @@ ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY")
 
 # 如果没有设置，则自动生成一个新的密钥并存储到环境变量
 if ENCRYPTION_KEY is None:
-    ENCRYPTION_KEY = Fernet.generate_key().decode()  # 使用Fernet生成的密钥
+    ENCRYPTION_KEY = Fernet.generate_key().decode()
     os.environ["ENCRYPTION_KEY"] = ENCRYPTION_KEY
 
-fernet = Fernet(ENCRYPTION_KEY.encode())  # 将密钥转换为字节类型
+fernet = Fernet(ENCRYPTION_KEY.encode())
 
 def compress_data(data: str) -> bytes:
     """压缩数据"""
@@ -27,9 +26,12 @@ def decompress_data(data: bytes) -> str:
 def encrypt_data(data: str) -> str:
     """加密数据"""
     try:
+        protocol = ""
+        if '://' in data:
+            protocol, data = data.split('://', 1)
         compressed_data = compress_data(data)
-        encrypted_data = fernet.encrypt(compressed_data)
-        return encrypted_data.decode()
+        encrypted_data = fernet.encrypt(compressed_data).decode()
+        return f"{protocol}://{encrypted_data}" if protocol else encrypted_data
     except Exception as e:
         logging.error(f"Encryption failed: {e}")
         raise  
@@ -37,8 +39,12 @@ def encrypt_data(data: str) -> str:
 def decrypt_data(data: str) -> Union[str, None]:
     """解密数据"""
     try:
+        protocol = ""
+        if '://' in data:
+            protocol, data = data.split('://', 1)
         decrypted_data = fernet.decrypt(data.encode())
-        return decompress_data(decrypted_data)
+        decompressed_data = decompress_data(decrypted_data)
+        return f"{protocol}://{decompressed_data}" if protocol else decompressed_data
     except InvalidToken:
         logging.error("Decryption failed: Invalid token.")
         return None 
